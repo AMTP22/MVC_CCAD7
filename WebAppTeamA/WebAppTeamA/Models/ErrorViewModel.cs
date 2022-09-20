29b9a246d6951b7c9ac4c5f93e032e5ef0819a30
Austin Pearson <113139928+AMTP22@users.noreply.github.com>
2022-09-20 15:12:50 -0400

    Add files via upload

---
+++ WebAppTeamA/WebAppTeamA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppTeamA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
