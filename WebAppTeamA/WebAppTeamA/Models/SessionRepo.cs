9c90ce399b838eec346ee76f31cef980be697ebb
Alex <alexwerner569@hotmail.com>
2022-09-21 06:51:06 -0700

    test2

--- WebAppTeamA/WebAppTeamA/Models/SessionRepo.cs
+++ WebAppTeamA/WebAppTeamA/Models/SessionRepo.cs
@@ -2,13 +2,8 @@
 {
     public class SessionRepo
     {
-        private List<Session> sessions = new List<Session>()
-        {
-            new Session { Id = 1, Title = "Contoso Denver", Seats = 100},
-            new Session { Id = 2, Title = "Contoso Austin", Seats = 100},
-            new Session { Id = 3, Title = "Contoso Seattle", Seats = 100},
-            new Session { Id = 4, Title = "Contoso New Yor", Seats = 100 }
-        };
+        private List<Session> sessions = new List<Session>();
+        
 
         public List<Session> GetSessions()
         {
